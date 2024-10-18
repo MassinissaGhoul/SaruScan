b391543a2ba9d0bd98a633a068e84e30b31b3002
ReDiXe <121853668+ReDiXe@users.noreply.github.com>
2024-10-18 20:58:10 +0200

    ajout modif bdd

--- Documentation/Conception_BDD/saruscan.sql
+++ Documentation/Conception_BDD/saruscan.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 06, 2024 at 04:25 PM
+-- Generation Time: Oct 18, 2024 at 06:56 PM
 -- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- PHP Version: 8.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,13 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int NOT NULL,
+  `id_comics` int DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id_category`, `id_comics`, `category`) VALUES
+(1, 2, 'Aventure'),
+(2, 2, 'Action'),
+(3, 2, 'Fantaisie'),
+(4, 3, 'Action'),
+(5, 3, 'Aventure'),
+(6, 3, 'Arts Martiaux'),
+(7, 4, 'Fantastique'),
+(8, 4, 'Drame'),
+(9, 4, 'Action'),
+(10, 5, 'Super-héros'),
+(11, 5, 'Action'),
+(12, 5, 'École'),
+(13, 6, 'Action'),
+(14, 6, 'Fantastique'),
+(15, 6, 'Horreur'),
+(16, 7, 'Fantastique'),
+(17, 7, 'Action'),
+(18, 7, 'Aventure'),
+(19, 8, 'Action'),
+(20, 8, 'Aventure'),
+(21, 8, 'Arts Martiaux'),
+(22, 9, 'Thriller'),
+(23, 9, 'Mystère'),
+(24, 9, 'Psychologique'),
+(25, 10, 'Aventure'),
+(26, 10, 'Action'),
+(27, 10, 'Fantastique'),
+(28, 11, 'Horreur'),
+(29, 11, 'Action'),
+(30, 11, 'Drame');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `chapter`
 --
 
 CREATE TABLE `chapter` (
   `id_chapter` int NOT NULL,
   `id_comics` int NOT NULL,
-  `title_chapter` varchar(255) NOT NULL,
+  `chapter_number` int NOT NULL,
   `view_count` int DEFAULT '0',
   `comics_path` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -47,9 +95,26 @@ CREATE TABLE `comics` (
   `id_comics` int NOT NULL,
   `title_comics` varchar(255) NOT NULL,
   `author` varchar(255) DEFAULT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `category` varchar(255) DEFAULT NULL
+  `image_path` varchar(200) DEFAULT NULL,
+  `create_at` date DEFAULT NULL,
+  `description` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `comics`
+--
+
+INSERT INTO `comics` (`id_comics`, `title_comics`, `author`, `image_path`, `create_at`, `description`) VALUES
+(2, 'One Piece', 'Eiichiro Oda', './src/mangas/one_piece.jpg', NULL, NULL),
+(3, 'Naruto', 'Masashi Kishimoto', NULL, NULL, NULL),
+(4, 'Attack on Titan', 'Hajime Isayama', NULL, NULL, NULL),
+(5, 'My Hero Academia', 'Kohei Horikoshi', NULL, NULL, NULL),
+(6, 'Demon Slayer', 'Koyoharu Gotouge', NULL, NULL, NULL),
+(7, 'Fullmetal Alchemist', 'Hiromu Arakawa', NULL, NULL, NULL),
+(8, 'Dragon Ball', 'Akira Toriyama', NULL, NULL, NULL),
+(9, 'Death Note', 'Tsugumi Ohba', NULL, NULL, NULL),
+(10, 'Hunter x Hunter', 'Yoshihiro Togashi', NULL, NULL, NULL),
+(11, 'Tokyo Ghoul', 'Sui Ishida', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -74,8 +139,9 @@ CREATE TABLE `comment` (
 --
 
 CREATE TABLE `favorite` (
-  `id_user` int NOT NULL,
-  `id_comics` int NOT NULL
+  `id_favorite` int NOT NULL,
+  `id_user` int DEFAULT NULL,
+  `id_comics` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -85,10 +151,11 @@ CREATE TABLE `favorite` (
 --
 
 CREATE TABLE `rate` (
-  `id_user` int NOT NULL,
-  `id_comics` int NOT NULL,
-  `rate` int DEFAULT NULL
-) ;
+  `id_rate` int NOT NULL,
+  `id_user` int DEFAULT NULL,
+  `id_comics` int DEFAULT NULL,
+  `rating` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -98,7 +165,7 @@ CREATE TABLE `rate` (
 
 CREATE TABLE `user` (
   `id_user` int NOT NULL,
-  `pseudo` varchar(255) NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `is_admin` tinyint(1) DEFAULT '0'
@@ -107,6 +174,13 @@ CREATE TABLE `user` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`),
+  ADD KEY `id_comics` (`id_comics`);
 
 --
 -- Indexes for table `chapter`
@@ -135,14 +209,16 @@ ALTER TABLE `comment`
 -- Indexes for table `favorite`
 --
 ALTER TABLE `favorite`
-  ADD PRIMARY KEY (`id_user`,`id_comics`),
+  ADD PRIMARY KEY (`id_favorite`),
+  ADD KEY `id_user` (`id_user`),
   ADD KEY `id_comics` (`id_comics`);
 
 --
 -- Indexes for table `rate`
 --
 ALTER TABLE `rate`
-  ADD PRIMARY KEY (`id_user`,`id_comics`),
+  ADD PRIMARY KEY (`id_rate`),
+  ADD KEY `id_user` (`id_user`),
   ADD KEY `id_comics` (`id_comics`);
 
 --
@@ -157,6 +233,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
 -- AUTO_INCREMENT for table `chapter`
 --
 ALTER TABLE `chapter`
@@ -166,13 +248,25 @@ ALTER TABLE `chapter`
 -- AUTO_INCREMENT for table `comics`
 --
 ALTER TABLE `comics`
-  MODIFY `id_comics` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_comics` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
   MODIFY `comment_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `favorite`
+--
+ALTER TABLE `favorite`
+  MODIFY `id_favorite` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rate`
+--
+ALTER TABLE `rate`
+  MODIFY `id_rate` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -183,6 +277,12 @@ ALTER TABLE `user`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`id_comics`) REFERENCES `comics` (`id_comics`);
 
 --
 -- Constraints for table `chapter`
@@ -203,15 +303,15 @@ ALTER TABLE `comment`
 -- Constraints for table `favorite`
 --
 ALTER TABLE `favorite`
-  ADD CONSTRAINT `favorite_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE,
-  ADD CONSTRAINT `favorite_ibfk_2` FOREIGN KEY (`id_comics`) REFERENCES `comics` (`id_comics`) ON DELETE CASCADE;
+  ADD CONSTRAINT `favorite_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `favorite_ibfk_2` FOREIGN KEY (`id_comics`) REFERENCES `comics` (`id_comics`);
 
 --
 -- Constraints for table `rate`
 --
 ALTER TABLE `rate`
-  ADD CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE,
-  ADD CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`id_comics`) REFERENCES `comics` (`id_comics`) ON DELETE CASCADE;
+  ADD CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`id_comics`) REFERENCES `comics` (`id_comics`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
